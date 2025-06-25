46d64ee633a7f42dcfbadbbfd316c2cd48fc9166
abeerrai01 <theabeerrai.gmail.com>
2025-06-25 15:16:18 +0530

    ADDED FIREBASE AUTH 3

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -10,7 +11,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAZxYtdpGgXjgcTFxFIJaIyCljNoaCgW_Q",
   authDomain: "pathsarthi-trust-admin.firebaseapp.com",
   projectId: "pathsarthi-trust-admin",
-  storageBucket: "pathsarthi-trust-admin.firebasestorage.app",
+  storageBucket: "pathsarthi-trust-admin.appspot.com",
   messagingSenderId: "267179352941",
   appId: "1:267179352941:web:27479d68e3ab233930f064",
   measurementId: "G-HCGN7WHQ41"
@@ -18,4 +19,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 const analytics = getAnalytics(app);
