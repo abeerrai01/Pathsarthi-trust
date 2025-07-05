aa9d5efa1d03b38e29b0c1870d548efbcdca130e
abeerrai01 <theabeerrai.gmail.com>
2025-07-06 01:22:03 +0530

    CERTIFICATION 5

--- src/components/Dashboard/CertificateGenerator.jsx
+++ src/components/Dashboard/CertificateGenerator.jsx
@@ -33,27 +33,85 @@ export default function CertificateGenerator() {
       <button onClick={generateImage} className="bg-green-600 text-white px-4 py-2 rounded ml-2">
         Download Certificate as PNG
       </button>
-      {/* Hidden Certificate Template */}
+
+      {/* Visible scaled-down preview */}
+      <div
+        style={{
+          maxWidth: "360px",
+          width: "100%",
+          height: "auto",
+          margin: "40px auto 0 auto",
+          position: "relative",
+          border: "1px solid #eee",
+          borderRadius: "8px",
+          overflow: "hidden",
+          boxShadow: "0 2px 12px rgba(0,0,0,0.08)",
+        }}
+      >
+        <div
+          style={{
+            width: "1086px",
+            height: "1536px",
+            backgroundImage: `url('/certs/appreciation.jpg')`,
+            backgroundSize: "cover",
+            position: "relative",
+            fontFamily: "'Playfair Display', Georgia, serif",
+            transform: "scale(0.33)",
+            transformOrigin: "top left",
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: "700px",
+              left: "0",
+              width: "100%",
+              textAlign: "center",
+              fontSize: "30px",
+              fontFamily: "'Playfair Display', Georgia, serif",
+              fontWeight: 600,
+              color: "#6b4f2a",
+              letterSpacing: "0.5px",
+              textTransform: "capitalize",
+            }}
+          >
+            {name}
+          </div>
+          <img
+            src={`https://api.qrserver.com/v1/create-qr-code/?data=https://pathsarthi.in/verify/${encodeURIComponent(name)}&size=120x120`}
+            alt="QR Code"
+            style={{
+              position: "absolute",
+              bottom: "65px",
+              right: "75px",
+              width: "100px",
+              height: "100px",
+            }}
+          />
+        </div>
+      </div>
+
+      {/* Hidden Certificate Template for PNG export */}
       <div
         id="cert-template"
         style={{
+          display: "none",
           width: "1086px",
           height: "1536px",
           backgroundImage: `url('/certs/appreciation.jpg')`,
           backgroundSize: "cover",
           position: "relative",
-          fontFamily: "Georgia, serif",
+          fontFamily: "'Playfair Display', Georgia, serif",
         }}
       >
-        {/* Name Placement */}
         <div
           style={{
             position: "absolute",
-            top: "750px",
+            top: "700px",
             left: "0",
             width: "100%",
             textAlign: "center",
-            fontSize: "50px",
+            fontSize: "30px",
             fontFamily: "'Playfair Display', Georgia, serif",
             fontWeight: 600,
             color: "#6b4f2a",
@@ -63,7 +121,6 @@ export default function CertificateGenerator() {
         >
           {name}
         </div>
-        {/* QR Code near Chairman signature */}
         <img
           src={`https://api.qrserver.com/v1/create-qr-code/?data=https://pathsarthi.in/verify/${encodeURIComponent(name)}&size=120x120`}
           alt="QR Code"
