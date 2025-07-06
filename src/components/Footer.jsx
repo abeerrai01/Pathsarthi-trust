92cfc787a37396a84e217b0e2829afe1c157b09c
abeerrai01 <theabeerrai.gmail.com>
2025-07-07 00:34:57 +0530

    linkedin

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -113,6 +113,17 @@ const Footer = () => {
                   />
                 </svg>
               </a>
+              <a
+                href="https://www.linkedin.com/company/path-sarthi-trust"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-300 hover:text-white"
+              >
+                <span className="sr-only">LinkedIn</span>
+                <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11 19h-3v-9h3v9zm-1.5-10.29c-.97 0-1.75-.79-1.75-1.75s.78-1.75 1.75-1.75 1.75.79 1.75 1.75-.78 1.75-1.75 1.75zm15.5 10.29h-3v-4.5c0-1.08-.02-2.47-1.5-2.47-1.5 0-1.73 1.17-1.73 2.38v4.59h-3v-9h2.89v1.23h.04c.4-.75 1.38-1.54 2.84-1.54 3.04 0 3.6 2 3.6 4.59v4.72z"/>
+                </svg>
+              </a>
             </div>
           </div>
         </div>
